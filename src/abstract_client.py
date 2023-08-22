d9297e6ff6c4474ec858676a75b81f1d66b0acdc
Ole626 <olepoeth@gmail.com>
2023-08-22 22:09:13 +0200

    Change broker connection for better class independence.

--- src/abstract_client.py
+++ src/abstract_client.py
@@ -1,5 +1,6 @@
 from abc import ABC, abstractmethod
 from paho.mqtt import client as mqtt_client
+from threading import Thread
 
 
 class AbstractClient(ABC):
@@ -10,13 +11,14 @@ class AbstractClient(ABC):
         self.id = f'client-{id}'
         self.client = None
         self.connected = False
+        self.latest_msg = None
 
     @abstractmethod
     def subscribe(self, topic):
         pass
 
     @abstractmethod
-    def publish(self, topic):
+    def publish(self, topic, msg):
         pass
 
     def connect_broker(self):
@@ -36,3 +38,9 @@ class AbstractClient(ABC):
         client.on_disconnect = on_disconnect
         client.connect(self.broker, self.port)
         self.client = client
+
+        thread = Thread(target=client.loop_forever)
+        thread.start()
+
+    def disconnect_broker(self):
+        self.client.disconnect()
--- src/display_client.py
+++ src/display_client.py
@@ -1,29 +1,16 @@
 from abstract_client import AbstractClient
 
 
-BROKER_IP = "192.168.0.101"
-BROKER_PORT = 1883
-TOPIC = "python/test"
-
-
 class DisplayClient(AbstractClient):
 
-    def publish(self, topic):
+    def publish(self, topic, msg):
         pass
 
     def subscribe(self, topic):
         def on_message(client, userdata, msg):
             print(f"Received `{msg.payload.decode()}` from `{msg.topic}` topic")
-            client.disconnect()
+            self.latest_msg = msg
 
         self.client.subscribe(topic)
         self.client.on_message = on_message
         print(f"Subscribed to {topic}")
-
-    @staticmethod
-    def main():
-        dis_client = DisplayClient(BROKER_IP, BROKER_PORT, "display")
-
-        dis_client.connect_broker()
-        dis_client.subscribe(TOPIC)
-        dis_client.client.loop_forever()
